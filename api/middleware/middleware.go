b8b1c6c7ec8d309adfcb371f10120da1c2cd9961
fazliddin <hajrullaevfazliddin058@gmail.com>
2024-09-30 20:19:29 +0500

    added cors error

--- api/middleware/middleware.go
+++ api/middleware/middleware.go
@@ -89,3 +89,21 @@ func PermissionMiddleware(enf *casbin.Enforcer) gin.HandlerFunc {
 		ctx.Next()
 	}
 }
+
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		log.Println("Cors middleware triggered")
+		c.Writer.Header().Set("Content-Type", "application/json")
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Max-Age", "86400")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE, UPDATE")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, X-Max")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(200)
+		} else {
+			c.Next()
+		}
+	}
+}
